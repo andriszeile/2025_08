f177299611914ef386c41bac9ab5f7655835786a
andriszeile <71541542+andriszeile@users.noreply.github.com>
2026-02-13 14:51:36 +0200

    111

---
+++ script_uzd/script.js
@@ -0,0 +1,6 @@
+//1.uzdevums
+
+function ievade() {
+    let vards = document.getElementById('varda_ievade').value;
+    document.getElementById('varda_izvade').innerHTML = "Tavs ierakstītais vārds ir "+ vards;
+}
